10781ff1c2fecf5863f0c3be56b2e9e64192aec7
Ben-Peters <49176877+Ben-Peters@users.noreply.github.com>
2020-02-25 10:14:41 -0500

    did some stuff doesnt seeem to work

--- HW4/src/LinkedList.h
+++ HW4/src/LinkedList.h
@@ -5,9 +5,45 @@
 #ifndef HW4_LINKEDLIST_H
 #define HW4_LINKEDLIST_H
 
+#include "Room.h"
 
 class LinkedList {
+    typedef struct
+    {
+        int roomNumber;
+        float treasure;
+    }SearchResults;
+    typedef Room Payload;
+    typedef SearchResults Payload2;
+    struct LLNode;
+    typedef struct
+    {
+        struct LLNode* next;
+        struct LLNode* prev;
+        Payload* payP;
+    }LLNode;
+    typedef struct
+    {
+        struct LLNode2* next;
+        struct LLNode2* prev;
+        Payload2* payP;
+    }LLNode2;
 
+    typedef struct
+    {
+        Payload* mp;
+        LLNode* newQHead;
+    }backFromDQFIFO;
+
+    LLNode* makeEmptyLinkedList();
+    LLNode2* makeEmptyLinkedList2();
+    LLNode* removeFromList(LLNode* hp, Payload* pP);
+    void savePayload(LLNode* lp, Payload* mp);
+    void savePayload2(LLNode2* lp, Payload2* mp);
+    bool isEmpty(LLNode* lp);
+    Payload* dequeueLIFO(LLNode* lp);
+    backFromDQFIFO* dequeueFIFO(LLNode* lp);
+    void printHistory(LLNode2* hp);
 };
 
 
