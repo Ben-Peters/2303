bf95f7563affcdcc3c9c8dbee0d5d85dcd524725
Ben-Peters <49176877+Ben-Peters@users.noreply.github.com>
2020-02-26 11:11:13 -0500

    Update LinkedList.h

--- HW4/src/LinkedList.h
+++ HW4/src/LinkedList.h
@@ -20,12 +20,12 @@ public:
         struct LLNode2 *prev;
         Payload2 *payP;
     } LLNode2;
-    struct LLNode;
-    typedef struct {
+    typedef struct LLNode;
+    struct LLNode{
         struct LLNode *next;
         struct LLNode *prev;
         Payload *payP;
-    } LLNode;
+    };
     typedef struct {
         Payload *mp;
         LLNode *newQHead;
@@ -34,7 +34,7 @@ public:
     virtual ~LinkedList();
     LLNode* makeEmptyLinkedList();
     LLNode2* makeEmptyLinkedList2();
-    struct LLNode * removeFromList(LLNode *hp, Payload *pP);
+    LLNode * removeFromList(LLNode *hp, Payload *pP);
     void savePayload(LLNode *lp, Payload *mp);
     void savePayload2(LLNode2 *lp, Payload2 *mp);
     bool isEmpty(LLNode *lp);
