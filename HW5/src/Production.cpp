c3f5b9cf99c1d30244733f3055923b0a7400fe96
Ben-Peters <49176877+Ben-Peters@users.noreply.github.com>
2020-03-03 22:49:49 -0500

    fixed game lodgic and auto formatted

--- HW5/src/Production.cpp
+++ HW5/src/Production.cpp
@@ -24,6 +24,7 @@ Production::~Production() {
 
 bool Production::prod(int argc, char *argv[]) {
     bool answer = false;
+    bool redTurn = false;
 
     if (argc <= 1) //no interesting information
     {
@@ -106,127 +107,135 @@ bool Production::prod(int argc, char *argv[]) {
         fflush(stdout);
         */
 
-        CheckerPiece* allPieces = (CheckerPiece*) malloc(sizeof(CheckerPiece)*24);
+        CheckerPiece *allPieces = (CheckerPiece *) malloc(sizeof(CheckerPiece) * 24);
         initBoard(allPieces);
 
 
-        for(int i = 0; i < numMoves; i++){
-            CheckerPiece::PossibleMove** possibleMoves;
-            possibleMoves = (CheckerPiece::PossibleMove**) malloc(24*sizeof(malloc(4*sizeof(possibleMoves))));
-            for(int j =0; j < 24; j++){
-                CheckerPiece::PossibleMove* tempPossibleMoves =(allPieces+j)->getAllPossibleMoves(allPieces);
-                    *(possibleMoves+j) = tempPossibleMoves;
-                }
+        for (int i = 0; i < numMoves; i++) {
+            int maxPiece = 12;
+            int minPiece = 0;
+            if (redTurn) {
+                maxPiece = 24;
+                minPiece = 12;
+            }
+            CheckerPiece::PossibleMove **possibleMoves;
+            possibleMoves = (CheckerPiece::PossibleMove **) malloc(12 * sizeof(malloc(4 * sizeof(possibleMoves))));
+            for (int j = minPiece; j < maxPiece; j++) {
+                CheckerPiece::PossibleMove *tempPossibleMoves = (allPieces + j)->getAllPossibleMoves(allPieces);
+                *(possibleMoves + j) = tempPossibleMoves;
+            }
             bool madeMove = false;
             srand(time(0));
-            while(!madeMove){
-                int pieceMoved = rand()%24;
-                if((*(possibleMoves+(pieceMoved)))-> newPiece != nullptr){
-                    makeMove(*(possibleMoves+(pieceMoved)));
-                    madeMove=true;
+            while (!madeMove) {
+                int pieceMoved = (rand() % 12) + minPiece;
+                int moveMade = (rand() % 4);
+                if ((*((*(possibleMoves + (pieceMoved))) + moveMade)).newPiece != nullptr) {
+                    makeMove(*(possibleMoves + (pieceMoved)));
+                    madeMove = true;
                 }
             }
+            redTurn = !redTurn;
         }
-        }
-        /*
-        //we'll want to conduct the search
-        //for the search we'll need an empty search history
-        LinkedList::LinkedListNode2 *historyP = LinkedList::makeEmptyLinkedList2();
-        //we'll need the Queue, into which we put rooms, and remove rooms
-        LinkedList::LinkedListNode *searchQ = LinkedList::makeEmptyLinkedList();
-        cout << ("starting search") << endl;
-        fflush(stdout);
-        //we'll start searching with room 0
-        bool done = false;
-        int searchedRooms = 0;
-        float foundTreasure = 0.0;
-        Room *roomBeingSearchedP = theRoomPs[0];
-        //we set its searched field to true, and take its treasure
-        roomBeingSearchedP->searched = true;
-        //we record it in the history
-        LinkedList::SearchResults *srP = (LinkedList::SearchResults *) malloc(sizeof(LinkedList::SearchResults));
-        srP->roomNumber = 0;
-        srP->treasure = roomBeingSearchedP->treasure;
-        if ((srP->treasure <= maxTreas) && (maxRooms > 0)) {
-            //here we are enqueueing room 0
-            cout << ("Enqueuing room 0") << endl;
-            LinkedList::savePayload(searchQ, roomBeingSearchedP);
+    }
+    /*
+    //we'll want to conduct the search
+    //for the search we'll need an empty search history
+    LinkedList::LinkedListNode2 *historyP = LinkedList::makeEmptyLinkedList2();
+    //we'll need the Queue, into which we put rooms, and remove rooms
+    LinkedList::LinkedListNode *searchQ = LinkedList::makeEmptyLinkedList();
+    cout << ("starting search") << endl;
+    fflush(stdout);
+    //we'll start searching with room 0
+    bool done = false;
+    int searchedRooms = 0;
+    float foundTreasure = 0.0;
+    Room *roomBeingSearchedP = theRoomPs[0];
+    //we set its searched field to true, and take its treasure
+    roomBeingSearchedP->searched = true;
+    //we record it in the history
+    LinkedList::SearchResults *srP = (LinkedList::SearchResults *) malloc(sizeof(LinkedList::SearchResults));
+    srP->roomNumber = 0;
+    srP->treasure = roomBeingSearchedP->treasure;
+    if ((srP->treasure <= maxTreas) && (maxRooms > 0)) {
+        //here we are enqueueing room 0
+        cout << ("Enqueuing room 0") << endl;
+        LinkedList::savePayload(searchQ, roomBeingSearchedP);
 
-        }
+    }
 
-        while (!done) {   //here we want to find all of the rooms adjacent to the roomBeingSearched,
-            //so we look in the adj matrix
-            for (int col = 0; (col < nrooms) && !done; col++) {
-                //we check whether this room is neighboring
-                printf("checking rooms %d and %d.\n", roomBeingSearchedP->roomNumber, col);
+    while (!done) {   //here we want to find all of the rooms adjacent to the roomBeingSearched,
+        //so we look in the adj matrix
+        for (int col = 0; (col < nrooms) && !done; col++) {
+            //we check whether this room is neighboring
+            printf("checking rooms %d and %d.\n", roomBeingSearchedP->roomNumber, col);
+            fflush(stdout);
+            if (AdjMatrix::getEdge(adjMP, roomBeingSearchedP->roomNumber, col) == 1) {
+                cout << ("found an edge") << endl;
                 fflush(stdout);
-                if (AdjMatrix::getEdge(adjMP, roomBeingSearchedP->roomNumber, col) == 1) {
-                    cout << ("found an edge") << endl;
-                    fflush(stdout);
-                    //if so, we check whether room has been searched
-                    if (!(theRoomPs[col]->searched)) {//if it hasn't been searched
-                        printf("Room %d hasn't already been searched.\n", col);
-                        //we set it to searched
-                        theRoomPs[col]->searched = true;
-                        if (!done)
-                            //we check whether we can take the treasure vs. limit
-                            //we check whether we've hit the room limit
-                        {//we enqueue it for search
-                            foundTreasure += roomBeingSearchedP->treasure;
-                            searchedRooms += 1;
-                            printf("found treasure updated to %f.\n", foundTreasure);
-                            printf("enqueuing room %d.\n", col);
-                            fflush(stdout);
-                            printf("Before enqueuing queue empty reports %d\n", LinkedList::isEmpty(searchQ));
-                            LinkedList::savePayload(searchQ, roomBeingSearchedP);
-                            srP = (LinkedList::SearchResults *) malloc(sizeof(LinkedList::SearchResults));
-                            srP->roomNumber = theRoomPs[col]->roomNumber;
-                            srP->treasure = theRoomPs[col]->treasure;
-                            LinkedList::savePayload2(historyP, srP);
-                            printf("After enqueuing, queue empty reports %d\n", LinkedList::isEmpty(searchQ));
-                        }//check about search limits
-                    }//room can still be searched
-                }//room is a neighbor
+                //if so, we check whether room has been searched
+                if (!(theRoomPs[col]->searched)) {//if it hasn't been searched
+                    printf("Room %d hasn't already been searched.\n", col);
+                    //we set it to searched
+                    theRoomPs[col]->searched = true;
+                    if (!done)
+                        //we check whether we can take the treasure vs. limit
+                        //we check whether we've hit the room limit
+                    {//we enqueue it for search
+                        foundTreasure += roomBeingSearchedP->treasure;
+                        searchedRooms += 1;
+                        printf("found treasure updated to %f.\n", foundTreasure);
+                        printf("enqueuing room %d.\n", col);
+                        fflush(stdout);
+                        printf("Before enqueuing queue empty reports %d\n", LinkedList::isEmpty(searchQ));
+                        LinkedList::savePayload(searchQ, roomBeingSearchedP);
+                        srP = (LinkedList::SearchResults *) malloc(sizeof(LinkedList::SearchResults));
+                        srP->roomNumber = theRoomPs[col]->roomNumber;
+                        srP->treasure = theRoomPs[col]->treasure;
+                        LinkedList::savePayload2(historyP, srP);
+                        printf("After enqueuing, queue empty reports %d\n", LinkedList::isEmpty(searchQ));
+                    }//check about search limits
+                }//room can still be searched
+            }//room is a neighbor
 
-                if (foundTreasure >= maxTreasure) {
-                    done = true;
-                    cout << ("Done by treasure") << endl;
-                }
-                if (searchedRooms >= maxRooms) {
-                    done = true;
-                    cout << ("Done by rooms") << endl;
-                }
-            }//scan for all possible rooms to search from this room
-            //time to get the next room
-            if (LinkedList::isEmpty(searchQ)) {
+            if (foundTreasure >= maxTreasure) {
                 done = true;
-                cout << ("Done by queue empty") << endl;
+                cout << ("Done by treasure") << endl;
             }
-            if (!done) {
-                cout << ("Invoking  dequeue") << endl;
-                fflush(stdout);
-                roomBeingSearchedP = LinkedList::dequeueLIFO(searchQ);
-
+            if (searchedRooms >= maxRooms) {
+                done = true;
+                cout << ("Done by rooms") << endl;
             }
-        }//while search is not done
-        //search is now done, time to print the history
-        LinkedList::printHistory(historyP);
+        }//scan for all possible rooms to search from this room
+        //time to get the next room
+        if (LinkedList::isEmpty(searchQ)) {
+            done = true;
+            cout << ("Done by queue empty") << endl;
+        }
+        if (!done) {
+            cout << ("Invoking  dequeue") << endl;
+            fflush(stdout);
+            roomBeingSearchedP = LinkedList::dequeueLIFO(searchQ);
+
+        }
+    }//while search is not done
+    //search is now done, time to print the history
+    LinkedList::printHistory(historyP);
 
 
-    }//end of else we have good arguments
+}//end of else we have good arguments
 */
     return answer;
 }
 
-void initBoard(CheckerPiece*&pieces){
-    for(int i  = 0; i < 3; i ++){
-        for(int j = 0; j < 4; j++){
-            *(pieces + (i*4) + j) = new Pawn(i, (2*j)+((i+1)%2), false);
+void initBoard(CheckerPiece *&pieces) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            *(pieces + (i * 4) + j) = new Pawn(i, (2 * j) + ((i + 1) % 2), false);
         }
     }
-    for(int i  = 0; i < 3; i ++){
-        for(int j = 0; j < 4; j++){
-            *(pieces + (i*4) + j) = new Pawn(i+5, (2*j)+(i%2), true);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            *(pieces + (i * 4) + j) = new Pawn(i + 5, (2 * j) + (i % 2), true);
         }
     }
 }
@@ -238,6 +247,6 @@ int Production::promptNumOfMoves() {
     return moves;
 }
 
-void MakeMove(CheckerPiece::PossibleMove* move){
+void MakeMove(CheckerPiece::PossibleMove *move) {
 
 }
